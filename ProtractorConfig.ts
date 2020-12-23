abae360671eebea6836d63df7039ac332dddad68
prashanttathe <70051910+prashanttathe@users.noreply.github.com>
2020-12-23 22:02:22 +0530

    Update ProtractorConfig.ts

--- ProtractorConfig.ts
+++ ProtractorConfig.ts
@@ -27,9 +27,9 @@ export let config: Config = {
 
   capabilities:
   {
-     browserName: 'firefox',
+     browserName: 'chrome',
 
-  'moz:firefoxOptions': {
+  'chromeOptions': {
      args: [ "--headless" ]
    }
   },
